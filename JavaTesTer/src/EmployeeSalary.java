4071fbf9866d5833b2bc3e1f4f7f001e34f16898
jukkrit.choo@gmail.com <jukkrit.choo@gmail.com>
2021-11-18 01:14:48 +0700

    can make salary v1 commit

--- JavaTesTer/src/EmployeeSalary.java
+++ JavaTesTer/src/EmployeeSalary.java
@@ -6,10 +6,10 @@ public class EmployeeSalary {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.println("sdada");
+
 		getStarted();
 	}
-	
+
 
 	public static void getStarted() {
 		Scanner kb = new Scanner(System.in);
@@ -17,37 +17,40 @@ public class EmployeeSalary {
 		int number = 0;
 		int count = 0;
 		number = kb.nextInt();
+		
 		String[] employ_name = new String[number];
 		int[] employ_wage = new int[number];
+		int[] employ_day = new int[number];
+		int[] employ_salary = new int[number];
 		System.out.println("Your number of employee is "+number);
 		// รับข้อมุลจาก user จำนวนของพนักงาน
 		for (int i = 0; i < number; i++) {
-			System.out.print("input Name of employee ");
-			employ_name[i] = kb.next();
-			System.out.print("input Wage of employee ");
-			employ_wage[i] = kb.nextInt();
 			count++;
+			System.out.print("input Name and Wage of employee and day for work "+"["+count+"] ");
+			employ_name[i] = kb.next(); 
+			employ_wage[i] = kb.nextInt(); 
+			employ_day[i] = kb.nextInt();
+			employ_salary[i] = getWork(employ_day[i],employ_wage[i]); // send work for day
+			
+			
 			
 			//output
 			if(count == number) {	
 				count =0;
-				/*System.out.println(Arrays.toString(employ_name));
-				System.out.println(Arrays.toString(employ_wage));*/
-				
 				for (int j = 0; j < number; j++) {
 					count++;
-					System.out.println("Employ["+count+"]  "+employ_name[j]+"  "+employ_wage[j]+" Bath");
-				
+					System.out.println("Employ["+count+"] ชื่อ => "+employ_name[j]+" ค่าจ้าง => "+employ_wage[j]+"บาท =>"+" วันที่ทำงาน =>"+employ_day[j]+"วัน เงินเดือนที่ได้รับ =>"+employ_salary[j]);
+
 				}
 
 			}
 		}
-		System.out.println("-------------- Table for wokr -------------- ");
-			
-		
-		
+	}
+	public static int getWork(int day,int wage) {
+			int work = day * wage;
+		return work;
 		
 	}
-	
+
 
 }
